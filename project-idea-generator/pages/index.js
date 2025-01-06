2b2d780abe99ae693aeaceaa8e56a4aa8e3084ee
acl13 <128934431+acl13@users.noreply.github.com>
2025-01-05 20:41:41 -0500

    generate random project ideas with app category and api choices

--- project-idea-generator/pages/index.js
+++ project-idea-generator/pages/index.js
@@ -1,28 +1,48 @@
 import { apiList } from "./apiList";
+import { appCategories } from "./appCategories";
 import { useState } from "react";
 
 export default function Home() {
   const [api, setApi] = useState({});
+  const [category, setCategory] = useState("");
 
   const getRandomApi = () => {
     const randomNum = Math.floor(Math.random() * 224) + 1;
     const randomApi = apiList.find((api) => api.id === randomNum);
-    console.log(randomApi);
     setApi(randomApi);
+  };
+
+  const getRandomCategory = () => {
+    const randomNum = Math.floor(Math.random() * 25);
+    const randomCategory = appCategories[randomNum];
+    setCategory(randomCategory);
+  };
+
+  const getRandomProjectIdea = () => {
+    getRandomApi();
+    getRandomCategory();
   };
 
   return (
     <div>
-      <button type="button" onClick={getRandomApi}>
-        Get Random API
-      </button>
-
-      <h2>
+      <h1>Random Project Idea Generator</h1>
+      {category !== "" ? (
+        <h2>
+          Build a <span className="italic">{category}</span> app using the
+          following API:
+        </h2>
+      ) : (
+        <h2>Press the button to generate a random API and App Category</h2>
+      )}
+      <h3>
         <a href={api.link} target="_blank">
           {api.name}
         </a>
-      </h2>
+      </h3>
       <p>{api.description}</p>
+      <button type="button" onClick={getRandomProjectIdea}>
+        Get Random Project Idea
+      </button>
     </div>
   );
 }
